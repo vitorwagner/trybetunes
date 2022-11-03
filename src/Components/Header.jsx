49150115fbf79a09377509245fa6355e00b761f8
Vitor Wagner <vitormwagner@gmail.com>
2022-11-03 17:36:16 -0300

    Requisito 4

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -22,6 +23,20 @@ class Header extends Component {
     return (
       <header data-testid="header-component">
         {loading ? <Loading /> : <h1 data-testid="header-user-name">{name}</h1>}
+        <nav>
+          <ul>
+            <li><Link data-testid="link-to-search" to="/search">Search</Link></li>
+            <li>
+              <Link
+                data-testid="link-to-favorites"
+                to="/favorites"
+              >
+                Favorites
+              </Link>
+            </li>
+            <li><Link data-testid="link-to-profile" to="/profile">Profile</Link></li>
+          </ul>
+        </nav>
       </header>
     );
   }
